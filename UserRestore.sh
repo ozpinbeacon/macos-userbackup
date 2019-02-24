543de35b32f464dbac622ca11fc54e22f6cda240
Liam Matthews <liamthesquirrel@gmail.com>
2019-02-25 09:42:50 +1100

    Added additional options for full and minimal backups and fixed some syntax issues with locations

--- UserRestore.sh
+++ UserRestore.sh
@@ -55,7 +55,12 @@ done
 echo Please type the name of the user you wish to restore.
 read username
 
-echo I am now going to restore ${users[$username]}
+echo Would you like to do a minimal backup or a full backup?
+echo 1. Minimal Backup
+echo 2. Full Backup
+read choice
+
+echo I am now going to restore "${users[$username]}"
 
 read -n 1 -s -r -p "Press any key to continue"
 
@@ -63,20 +68,34 @@ read -n 1 -s -r -p "Press any key to continue"
 # Creating a mobile account for the user
 
 sleep 2s
-sudo /System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n ${users[$username]}
+sudo /System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n "${users[$username]}"
 sleep 2s
 
 ###############################################
 # Restoring the data
 
-sudo rsync -aEh --progress /Volumes/${locations[$backup]}/Users/${users[$username]} /Users/
+if [ $choice == "1" ]
+then
+    sudo rsync -aEh --progress /Volumes/"${locations[$backup]}"/Users/"${users[$username]}"/Desktop /Users/"${users[$username]}"/
+    sudo rsync -aEh --progress /Volumes/"${locations[$backup]}"/Users/"${users[$username]}"/Documents /Users/"${users[$username]}"/
+    sudo rsync -aEh --progress /Volumes/"${locations[$backup]}"/Users/"${users[$username]}"/Downloads /Users/"${users[$username]}"/
+    sudo rsync -aEh --progress /Volumes/"${locations[$backup]}"/Users/"${users[$username]}"/Movies /Users/"${users[$username]}"/
+    sudo rsync -aEh --progress /Volumes/"${locations[$backup]}"/Users/"${users[$username]}"/Pictures /Users/"${users[$username]}"/
+    sudo rsync -aEh --progress /Volumes/"${locations[$backup]}"/Users/"${users[$username]}"/Music /Users/"${users[$username]}"/
+elif [ $choice == "2" ]
+then
+    sudo rsync -aEh --progress /Volumes/"${locations[$backup]}"/Users/"${users[$username]}" /Users/
+else
+    echo "Choice not recgonized, please run script again."
+    exit 1
+fi
 
 echo Finished restoring user, now restoring ownership.
 
 ###############################################
 # Changing of ownership to allow for use
 
-sudo chown -R ${users[$username]}:admin /Users/${users[$username]}
+sudo chown -R "${users[$username]}":admin /Users/"${users[$username]}"
 
 ###############################################
 # Removing configuration profile
@@ -84,3 +103,5 @@ sudo chown -R ${users[$username]}:admin /Users/${users[$username]}
 sudo profiles remove -identifier=B48A6E5B-BAA3-4E30-97A2-7B83AA708A8D
 
 echo Restoration complete!
+
+exit 0
