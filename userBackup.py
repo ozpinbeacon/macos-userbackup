9f80ecef7ab105504f78154d8a86574bdef8472d
Liam Matthews <liam@ethernetisoverrated.com>
2021-12-10 09:22:16 +1100

    Should be done

--- userBackup.py
+++ userBackup.py
@@ -2,6 +2,7 @@
 
 import argparse
 import subprocess
+import pwd
 import re
 import os
 import sys
@@ -15,21 +16,54 @@ args = parser.parse_args()
 
 standardCopy = ['Desktop', 'Documents', 'Downloads', 'Pictures', 'Music', 'Movies']
 
-def argumentValidation():
-	if args.type != 'standard' and args.type != 'full':
-		print('ERROR: Type of backup not recognized')
-		print('Please use either "standard" or "full" with "-t/--type"')
+def environmentValidation():
+	errorEncountered = False
+
+	if args.type != 'standard' and args.type != 'full' and args.type != None:
+		print('ERROR: Type of backup not recognized', file=sys.stderr)
+		print('Please use either "standard" or "full" with "-t/--type"\n', file=sys.stderr)
 		parser.print_help(sys.stderr)
+		errorEncountered = True
 
 	if args.action != 'backup' and args.action != 'restore' and args.action != 'direct':
-		print('ERROR: Backup/restore action not recognized')
-		print('Please use with "backup", "restore" or "direct" with "-a/--action"')
+		print('ERROR: Backup/restore action not recognized', file=sys.stderr)
+		print('Please use with "backup", "restore" or "direct" with "-a/--action"\n', file=sys.stderr)
 		parser.print_help(sys.stderr)
+		errorEncountered = True
+
+	if not os.path.exists(args.location):
+		print("ERROR: Given '--location' does not exist", file=sys.stderr)
+		print("Please check the backup drive/location and run again\n", file=sys.stderr)
+		parser.print_help(sys.stderr)
+		errorEncountered = True
+	
+	if args.action == 'backup' or args.action == 'direct':
+		try:
+			pwd.getpwnam(args.username)
+		except KeyError:
+			print("ERROR: User given is not recognized, please check the username and try again\n", file=sys.stderr)
+			parser.print_help(sys.stderr)
+			errorEncountered = True
+
+	if not os.geteuid() == 0:
+		print("ERROR: This script must be run with root privileges in order to move user home folder, please run with sudo and try again", file=sys.stderr)
+		errorEncountered = True 
+
+	try:
+		temp = open('/Library/Application Support/com.apple.TCC/tcc.db')
+		temp.close()
+	except:
+		print('ERROR: Terminal requires full-disk access in order to perform this script, please give Terminal full-disk access in Security and Privacy and try again', file=sys.stderr)
+		errorEncountered = True
+		
+	if errorEncountered:
+		exit(1)
 
 def backup(location, type, username):
+	userPath = os.path.expanduser('~' + username)
 	backupPath = location + '/' + username
 
-	if os.path.exist(backupPath):
+	if os.path.exists(backupPath):
 		print("WARNING: User already exists at" + backupPath)
 		print("Are you sure you wish to overwrite the existing folder (y/n)")
 		option = input()
@@ -40,24 +74,33 @@ def backup(location, type, username):
 		os.rmdir(backupPath)
 
 	if type == "full":
-		subprocess.call(['rsync', '-aeh', '--progress', '/Users/' + username, backupPath])
+		subprocess.call(['rsync', '-aeh', '--progress', userPath, backupPath])
 	else:
 		for folder in standardCopy:
-			subprocess.call(['rsync', '-aeh', '--progress', '/Users/' + username, backupPath + '/' + folder])
+			subprocess.call(['rsync', '-aeh', '--progress', userPath + '/' + folder, backupPath + '/' + folder])
+	
+	f = open(backupPath + '/' + username + '-path', 'w')
+	f.write(userPath)
+	f.close()
 
 def restore(location, type, username):
 	backupPath = location + '/' + username
-	restorePath = '/Users/' + username
+	if os.path.isfile(backupPath + '/' + username + '-path'):
+		f = open(backupPath + '/' + username + '-path', 'r')
+		restorePath = f.read()
+		f.close()
+	else:
+		restorePath = '/Users/' + username
 
-	if os.path.exist(restorePath):
+	if os.path.exists(restorePath):
 		print("WARNING: User already exists at " + restorePath)
 		print("Are you sure you wish to overwrite the existing folder (y/n)")
 		option = input()
 		if not re.search(r'[yY]', option):
 			print('INFO: Not overwriting existing user folder, exiting...')
 			exit(1)
-	else:
-		os.rmdir(restorePath)
+		else:
+			os.rmdir(restorePath)
 	
 	if type == "full":
 		subprocess.call(['rsync', '-aeh', '--progress', backupPath, restorePath])
@@ -66,15 +109,7 @@ def restore(location, type, username):
 			subprocess.call(['rsync', '-aeh', '--progress', backupPath + '/' + folder, restorePath + '/' + folder])
 	
 def main():
-	argumentValidation()
-
-	try:
-		temp = open('/Library/Application Support/com.apple.TCC/tcc.db')
-		temp.close()
-	except:
-		print('Terminal requires full-disk access in order to perform this script')
-		exit(1)
-
+	environmentValidation()
 
 	if args.type == None:
 		type = "standard"
